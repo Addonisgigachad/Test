7752673f832de7b0d02228f4df4f5d30aac56335
Devin Lucas <addonisgigachad@gmail.com>
2024-05-14 11:26:58 +0200

    Variable obereeGrenze=15

--- Main.java
+++ Main.java
@@ -2,14 +2,15 @@ public class Main
 {
     public static void main()
     {
+        int obereGrenze=15;
         int summe=0;
-        for(int i=1;i<=10;++i)
+        for(int i=1;i<=obereGrenze;++i)
         {
             summe+=i;
         }
         
         int mult=1;
-        for(int i=2;i<=10;++i)
+        for(int i=2;i<=obereGrenze;++i)
         {
             mult*=i;
         }
