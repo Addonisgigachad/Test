512f21d6154668bf054c5d509ef29eeff996147d
Devin Lucas <addonisgigachad@gmail.com>
2024-05-14 11:07:59 +0200

    Multiplikation implementiert

--- Main.java
+++ Main.java
@@ -3,9 +3,14 @@ public class Main
     public static void main()
     {
         int summe=0;
-        for(int i=0;i<=10;++i)
+        for(int i=1;i<=10;++i)
         {
             summe+=i;
+        }
+        int mult=1;
+        for(int i=2;i<=10;++i)
+        {
+            mult*=i;
         }
     }
 }
